3f2f7c9e2720abddf03d7d16e9132cd5e41cd7e4
Thomas Cahill <cahill89@gmail.com>
2025-07-16 15:33:53 -0500

    surface & test file

--- surface.py
+++ surface.py
@@ -1,9 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from play import LinearRegression
+
+# import parameters
+linear_regression = LinearRegression()
+
+y = linear_regression.targets
+x = linear_regression.scaled_features
+
+# create meshgrid
+w = np.linspace(-100, 100, 100)
+b = np.linspace(-100, 100, 100)
+W, B = np.meshgrid(w,b)
 
 
 
-print("Hello World.")
+# compute Y_hat for each (w, b) pair
+# W and B are (100, 100), we want to compute predictions for each (w, b) across all x
+# So we expand W and B to (100, 100, 1) and x to (1, 1, 100)
+W_exp = W[:, :, np.newaxis] # shape: (100, 100, 1)
+B_exp = B[:, :, np.newaxis] # shape: (100, 100, 1)
+x_exp = x[np.newaxis, np.newaxis, :] # shape: (1, 1, 100)
+y_exp = y[np.newaxis, np.newaxis, :] # shape: (1, 1, 100)
 
-print("Test Change")
+# Compute predictions: (100, 100, 100)
+Y_hat = W_exp * x_exp + B_exp
+
+# Compute loss for each (w, b) pair
+Z = np.mean((y_exp - Y_hat) ** 2, axis=2)  # shape: (100, 100)
+
+
+print(Z.shape)
+
+# Plot the surface
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(W, B, Z, cmap='viridis')
+ax.set_xlabel('w')
+ax.set_ylabel('b')
+ax.set_zlabel('Loss')
+ax.set_title('Loss Surface')
+plt.show()
--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 #test
+import numpy as np
+
+
+a = np.array([1, 2]) # shape(2, )
+b = np.array([3, 4]) # shape(2, )
+
+A, B = np.meshgrid(a, b) # shape (2, 2)
+
+print(A)
+print(B)
+print("\n")
+
+A = A[:, :, np.newaxis]
+
+print(A)
+print(A.shape)
